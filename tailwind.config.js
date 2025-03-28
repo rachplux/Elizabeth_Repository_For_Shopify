77e6e8eb9d01970752a3efb9ae430f4e4b102f55
rachael <rach.plux@gmail.com>
2025-03-28 15:29:30 +0800

    okay version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+/* @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './layout/*.liquid',
-    './sections/*.liquid',
-    './snippets/*.liquid',
-    './templates/*.liquid',
-    './templates/customers/*.liquid',
-  ], // Look into layout folder, and find file ends with .liquid
+	content: [
+	  './layout/*.liquid',
+	  './sections/*.liquid',
+	  './snippets/*.liquid',
+	  './templates/*.{liquid,json}',
+	  './templates/customers/*.liquid',
+	  './src/**/*.{js,ts,jsx,tsx,css}', // 加入 src 內所有 JS、TS、CSS 檔案
+	], // Look into layout folder, and find file ends with .liquid
 
   theme: {
     extend: {},
