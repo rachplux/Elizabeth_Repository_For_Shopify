f7d9e0fa50f479739dd3d3fe00367a6ee4fa6266
rachael <rach.plux@gmail.com>
2025-03-28 11:07:32 +0800

    tailwindcss upgrad V.4

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,9 @@ module.exports = {
     './layout/*.liquid',
     './sections/*.liquid',
     './snippets/*.liquid',
-    './templates/*.liquid',
+    './templates/*.{liquid,json}',
     './templates/customers/*.liquid',
+	'./src/**/*.{js,ts,jsx,tsx,css}', // 加入 src 內所有 JS、TS、CSS 檔案
   ], // Look into layout folder, and find file ends with .liquid
 
   theme: {
