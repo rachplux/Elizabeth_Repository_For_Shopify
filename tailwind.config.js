8feb7a3d4137040a00b307a4cb4ada395076228d
rachael <rach.plux@gmail.com>
2025-04-11 17:00:44 +0800

    template-product & product-media

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
 	], // Look into layout folder, and find file ends with .liquid
 
   theme: {
-    extend: {},
+    extend: {
+		height: {
+			"94": '22rem'
+		}
+	},
   },
   plugins: [],
 };
